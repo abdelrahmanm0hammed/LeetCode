d180c55e4107c81c380a65257c191ab269671b04
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2025-12-23 22:36:36 +0200

    adding list function to convert string to list

--- Roman_to_Integer.py
+++ Roman_to_Integer.py
@@ -1,48 +1,59 @@
-
-
 def roman_to_integer(roman_num):
+    char = list(roman_num)
     result = 0
     i = 0
 
-    while i < len(roman_num):
-        if roman_num[i] == "I":
-            if i + 1 < len(roman_num) and roman_num[i+1] == "V":
-                result += 4
-                i += 2
-            elif i + 1 < len(roman_num) and roman_num[i+1] == "X":
-                result += 9
-                i += 2
+    while i < len(char):
+        if char[i] == "I":
+            if i + 1 < len(char):
+                if char[i + 1] == "V":
+                    result += 4
+                    i += 2
+                elif char[i + 1] == "X":
+                    result += 9
+                    i += 2
+                else:
+                    result += 1
+                    i += 1
             else:
                 result += 1
                 i += 1
 
-        elif roman_num[i] == "V":
+        elif char[i] == "V":
             result += 5
             i += 1
 
-        elif roman_num[i] == "X":
-            if i + 1 < len(roman_num) and roman_num[i+1] == "L":
-                result += 40
-                i += 2
-            elif i + 1 < len(roman_num) and roman_num[i+1] == "C":
-                result += 90
-                i += 2
+        elif char[i] == "X":
+            if i + 1 < len(char):
+                if char[i + 1] == "L":
+                    result += 40
+                    i += 2
+                elif char[i + 1] == "C":
+                    result += 90
+                    i += 2
+                else:
+                    result += 10
+                    i += 1
             else:
                 result += 10
                 i += 1
 
-        elif roman_num[i] == "C":
-            if i + 1 < len(roman_num) and roman_num[i+1] == "D":
-                result += 400
-                i += 2
-            elif i + 1 < len(roman_num) and roman_num[i+1] == "M":
-                result += 900
-                i += 2
+        elif char[i] == "C":
+            if i + 1 < len(char):
+                if char[i + 1] == "D":
+                    result += 400
+                    i += 2
+                elif char[i + 1] == "M":
+                    result += 900
+                    i += 2
+                else:
+                    result += 100
+                    i += 1
             else:
                 result += 100
                 i += 1
 
-        elif roman_num[i] == "D":
+        elif char[i] == "D":
             result += 500
             i += 1
 
@@ -51,3 +62,5 @@ def roman_to_integer(roman_num):
             i += 1
 
     return result
+
+print(roman_to_integer("IVVI"))
